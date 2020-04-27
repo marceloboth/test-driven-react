4bfd942fa46ebdfb4994b1bd513070cdd109fcaa
Marcelo J. Both <marcelo.both@gmail.com>
2020-04-26 22:40:58 -0300

    Initial esling and prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    node: true,
+  },
+  rules: {
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  settings: {
+    react: {
+      version: '16.4.2',
+    },
+  },
+};
